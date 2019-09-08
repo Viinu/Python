3819c3012d81bbb43ae7829fae1d4fc9a1c5e32d
Viinu_07 <41299419+Viinu07@users.noreply.github.com>
2019-09-08 22:36:15 +0530

    Create reversewords.py

---
+++ reversewords.py
@@ -0,0 +1,8 @@
+def printReverse(s): 
+    l = [s for s in s.split(' ')]  
+    print(l[0], end = ' ') 
+    for i in range(1, len(l)-1): 
+        print(l[i][::-1], end = ' ') 
+    print(l[len(l)-1]) 
+s = str(input())
+printReverse(s) 
